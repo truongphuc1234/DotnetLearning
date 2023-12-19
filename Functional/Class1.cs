30142c7c4b08b0ae88039b4a1af00412640a3be2
Phuc Pham <truongphudn@gmail.com>
2023-12-19 22:23:20 +0700

    Update Builder

--- Functional/Class1.cs
+++ Functional/Class1.cs
@@ -1,6 +1,32 @@
-﻿namespace Functional;
+﻿
+namespace Functional;
 
-public class Class1
+public class Film
 {
 
+    public string? Genre;
+}
+
+public class DataStore
+{
+    public IEnumerable<Film> GetFilmsByGenre(string genre)
+    {
+        var allFilms = GetAllFirms();
+        var chosenFilms = new List<Film>();
+        foreach (var f in allFilms)
+        {
+            if (f.Genre == genre)
+            {
+                chosenFilms.Add(f);
+            }
+        }
+        return chosenFilms;
+    }
+
+    public IEnumerable<Film> GetFilmsByGenre2(IEnumerable<Film> source, string genre) => source.Where(x => x.Genre == genre);
+
+    private List<Film> GetAllFirms()
+    {
+        throw new NotImplementedException();
+    }
 }
