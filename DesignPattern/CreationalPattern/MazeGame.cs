b0d38d1e16e6560bbc29a5b4ceee6d87b9a31676
Phuc Pham <truongphudn@gmail.com>
2023-12-20 16:26:54 +0700

    Builder

--- DesignPattern/CreationalPattern/MazeGame.cs
+++ DesignPattern/CreationalPattern/MazeGame.cs
@@ -1,27 +1,48 @@
+
 namespace CreationalPattern;
 
 public class MazeGame
 {
     public Maze CreateMaze()
     {
-        var maze = new Maze();
-        var r1 = new Room(1);
-        var r2 = new Room(2);
-        var door = new Door(r1, r2);
+        var maze = MakeMaze();
+        Room r1 = MakeRoom(1);
+        var r2 = MakeRoom(2);
+        var door = MakeDoor(r1, r2);
         maze.AddRoom(r1);
         maze.AddRoom(r2);
 
-        r1.SetSide(Direction.North, new Wall());
+        r1.SetSide(Direction.North, MakeWall());
         r1.SetSide(Direction.East, door);
-        r1.SetSide(Direction.South, new Wall());
-        r1.SetSide(Direction.West, new Wall());
+        r1.SetSide(Direction.South, MakeWall());
+        r1.SetSide(Direction.West, MakeWall());
 
-        r2.SetSide(Direction.North, new Wall());
-        r2.SetSide(Direction.East, new Wall());
-        r2.SetSide(Direction.South, new Wall());
+        r2.SetSide(Direction.North, MakeWall());
+        r2.SetSide(Direction.East, MakeWall());
+        r2.SetSide(Direction.South, MakeWall());
         r2.SetSide(Direction.West, door);
 
         return maze;
+    }
+
+    private Maze MakeMaze()
+    {
+        return new Maze();
+    }
+
+    protected virtual Room MakeRoom(int roomNo)
+    {
+        return new Room(roomNo);
+    }
+
+    protected virtual Door MakeDoor(Room roomFrom, Room roomTo)
+    {
+        return new Door(roomFrom, roomTo);
+    }
+
+    protected virtual Wall MakeWall()
+    {
+        return new Wall();
     }
 
     public Maze CreateMaze(MazeFactory factory)
